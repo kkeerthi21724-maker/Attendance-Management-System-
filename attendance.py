2dd6eb957af66ebdc6b8c3d43ad2282022df4345
kkeerthi21724-maker <kkeerthi21724@gmail.com>
2026-05-22 15:47:22 +0530

    Create attendance.py

---
+++ attendance.py
@@ -0,0 +1,12 @@
+print("===== Attendance Management System =====")
+
+name = input("Enter Student Name: ")
+roll = input("Enter Roll Number: ")
+attendance = input("Enter Attendance (Present/Absent): ")
+
+print("\n----- Attendance Details -----")
+print("Student Name :", name)
+print("Roll Number  :", roll)
+print("Attendance   :", attendance)
+
+print("\nAttendance Recorded Successfully!")
